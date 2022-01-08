aeff8051be5136f35654e2f9832126f3e01c39cd
KuniTatsu <kunitatsuvantan@gmail.com>
2022-01-08 16:06:59 +0900

    firstcodecommit

---
+++ program/game/game_main.cpp
@@ -0,0 +1,19 @@
+#include "DxLib.h"
+#include "../library/t2klib.h"
+#include "../support/Support.h"
+#include "game_main.h"
+#include <time.h>
+#include <list>
+#include <map>
+#include <algorithm>
+
+bool init = false;
+
+void gameMain(float deltatime) {
+
+	if (!init) {
+		init = true;
+	}
+
+}
+
